a1f8922513fbf4c5fa76696d3baa57fdbe337a78
Dixit <sdd929036@tatatechnologies.com>
2024-05-10 14:39:29 +0530

    Positional non-keywords args , open and read file in python

---
+++ self/positional_non-keywordargs.py
@@ -0,0 +1,8 @@
+#positional or non-keyword args
+def area(a,b) :
+    return a*b
+print(area(2,5)) #postional and non-keyword args
+
+print(area(a=3,b=4)) # Keyword args 
+print(area(b=5,a=4)) # for keyword args position actually doesn't matters
+
